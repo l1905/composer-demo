92e2a2299b45d79a6f1f227230cc53d4af053b1b
litong <litong@smzdm.com>
2019-01-15 13:37:29 +0800

    add 1.1.2

--- src/MainClient.php
+++ src/MainClient.php
@@ -27,4 +27,14 @@ class MainClient {
         echo "testMethod----echo".PHP_EOL;
     }
 
+    /**
+     * Author: litong@smzdm.com
+     * Date: 2019/1/15 下午1:23
+     * version : 1.1.2
+     */
+    function testMethod03()
+    {
+        echo "testMethod----echo".PHP_EOL;
+    }
+
 }
