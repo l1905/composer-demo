e78824bf9dc609db9d588faa075d1f73ca19ee5e
litong <litong@smzdm.com>
2019-01-15 13:04:38 +0800

    first-commit

---
+++ src/MainClient.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace litong\composer_demo;
+/**
+ * Author: litong@smzdm.com
+ * Date: 2019/1/15 上午11:52
+ */
+
+class MainClient {
+    function __construct()
+    {
+        echo "main client init";
+    }
+
+    function testMethod()
+    {
+        echo "testMethod----echo".PHP_EOL;
+    }
+
+
+}
